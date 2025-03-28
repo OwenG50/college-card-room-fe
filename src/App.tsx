45b33e4deb71a22958dcddc9073f3ad7bf1cd68b
OwenG50 <89798562+OwenG50@users.noreply.github.com>
2025-03-28 16:33:15 -0400

    working full stack application base

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState<string>("");
+
+  useEffect(() => {
+    const fetchMessage = async () => {
+      try {
+        const response = await fetch("http://localhost:5177/api/Hello");
+
+        if (!response.ok) {
+          throw new Error(`API call failed with status: ${response.status}`);
+        }
+
+        const data = await response.text();
+        setMessage(data);
+        
+      } catch (error) {
+        console.error(error);
+        setMessage("Error fetching message from server");
+      }
+    }
+
+    fetchMessage();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Poker App</h1>
+      <p>Message from API: {message}</p>
     </div>
   );
 }
